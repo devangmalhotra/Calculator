26de81de90e55c9c48a0eb106de005af4c79804d
devangmalhotra <malh2226@mylaurier.ca>
2024-08-24 23:21:30 -0400

    updated script

--- script.js
+++ script.js
@@ -18,3 +18,115 @@ let addButton = document.getElementById('button-add');
 let button0 = document.getElementById('button0');
 let decimalButton = document.getElementById('button-decimal');
 let equalsButton = document.getElementById('button-equals');
+let result = 0;
+let expression = "";
+
+acButton.onclick = clear;
+negativePositiveButton.onclick = positiveToNegative;
+percentageButton.onclick = convertToPercentage;
+divideButton.onclick = divide;
+button7.onclick = button7Operation;
+button8.onclick = button8Operation;
+button9.onclick = button9Operation;
+multiplyButton.onclick = multiply;
+button4.onclick = button4Operation;
+button5.onclick = button5Operation;
+button6.onclick = button6Operation;
+subtractButton.onclick = subtract;
+button1.onclick = button1Operation;
+button2.onclick = button2Operation;
+button3.onclick = button3Operation;
+addButton.onclick = add;
+button0.onclick = button0Operation;
+decimalButton.onclick = addDecimal;
+equalsButton.onclick = evaluateExpression;
+
+function update(result) {
+    let strResult = result.toString();
+    resultsScreen.innerText = strResult;
+    console.log(strResult);
+}
+
+function clear() {
+    expression = "";
+    result = 0;
+    resultsScreen.innerText = "0";
+}
+
+function positiveToNegative(result) {
+    if (result != 0) {
+        result *= -1;
+    }
+
+    update(result);
+
+    return result;
+}
+
+function convertToPercentage() {
+    
+}
+
+function divide() {
+
+}
+
+function button7Operation() {
+
+}
+
+function button8Operation() {
+
+}
+
+function button9Operation() {
+
+}
+
+function multiply() {
+
+}
+
+function button4Operation() {
+
+}
+
+function button5Operation() {
+
+}
+
+function button6Operation() {
+
+}
+
+function subtract() {
+
+}
+
+function button1Operation() {
+
+}
+
+function button2Operation() {
+
+}
+
+function button3Operation() {
+
+}
+
+function add() {
+
+}
+
+function button0Operation() {
+
+}
+
+function addDecimal() {
+
+}
+
+function evaluateExpression() {
+
+}
